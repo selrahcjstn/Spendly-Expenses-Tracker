387fc4310481af96854f5f91f3994939529a82b5
Charles Justine Mantes <charlesjustinemantes.main@gmail.com>
2025-10-02 10:00:14 +0800

    User controller created

---
+++ Spendly.Server/Solution1/Spendly.Api/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spendly.Application.Dtos;
+using Spendly.Application.Interfaces.IServices;
+
+namespace Spendly.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService userService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<UserResponseDto>> GetById(Guid id)
+        {
+            try
+            {
+                var result = await _userService.GetByIdAsync(id);
+
+                return Ok(result);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserResponseDto>> Create([FromBody] UserCreateRequestDto dto)
+        {
+            try
+            {
+                var result = await _userService.AddAsync(dto);
+
+                return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<UserResponseDto>> UpdateEmail(Guid id, [FromBody] UpdateEmailRequestDto dto)
+        {
+            try
+            {
+                dto.Id = id;
+                var result = await _userService.UpdateEmailAsync(dto);
+
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
